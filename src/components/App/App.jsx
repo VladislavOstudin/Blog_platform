52a6c735ba656cb327c6c6fce2ffc1e5af82117b
Vlad Ostudin <ostudin.v@gmail.com>
2025-05-01 22:40:39 +0300

    test husky

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,10 @@
 import './reset.css'
 
 export default function App() {
+  console.log('hello')
   return (
     <main>
       <h1>Blog platform</h1>
-      <h3>Blog platform</h3>
-      <h3>Blog platform</h3>
       <h3>Blog platform</h3>
     </main>
   )
