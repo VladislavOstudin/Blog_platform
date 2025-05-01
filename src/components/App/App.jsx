11044ae7c9c1354bc1e041da36d534148b9d3e08
Vlad Ostudin <ostudin.v@gmail.com>
2025-05-01 23:04:34 +0300

    test husky

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,13 +6,6 @@ export default function App() {
     <main>
       <h1>Blog platform</h1>
       <h3>Blog platform</h3>
-      <h3>Blog platform</h3>
-      <h3>Blog platform</h3>
-      <ul>
-        <li>1q1</li>
-        <li>2a2</li>
-        <li>3z3</li>
-      </ul>
     </main>
   )
 }
